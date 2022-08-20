40282790e46ed1b307f5ec294c29ade0595ce719
NikolaVasilev <nikola@madcorp.info>
2022-08-20 11:58:39 +0300

    added seed method, json data and also did some refactoring

---
+++ helpers.py
@@ -0,0 +1,79 @@
+import inspect
+import json
+import sys
+
+import pyautogui
+
+from WeaponFactoryOOP.class_library import Weapon, FireArms, ColdBladedWeapon, Axe, Sword
+
+
+def seed(list_of_weapon_objects):
+    create_menu, class_dict = get_classes_dictionary()
+
+    with open('WeaponFactoryOOP/weapon_list.json') as file:
+        data = json.load(file)
+
+        for weapon_class in data:
+            class_name, weapon_objects = list(weapon_class.items())[0]
+            instance_weapon_class = [weapon_class for weapon_class in class_dict if weapon_class[0] == class_name][0][1]
+            for weapon_object in weapon_objects:
+                instance_object = instance_weapon_class(*weapon_object.values())
+                list_of_weapon_objects.append(instance_object)
+
+    return list_of_weapon_objects
+
+
+def get_classes_dictionary():
+    cls_members = [member for member in inspect.getmembers(sys.modules[__name__], inspect.isclass) if
+                   member[1].__module__ == "WeaponFactoryOOP.class_library"]
+    classes = dict()
+
+    for cls in cls_members:
+        args = inspect.getfullargspec(cls[1].__init__)[0]
+        args.remove('self')
+        classes[cls[1].__doc__] = args
+
+    return classes, cls_members
+
+
+def create_instance_by_index(index: str, class_dict: list, create_menu: dict, list_of_weapon_objects: list):
+    class_instance = class_dict[int(index) - 1][1]
+
+    args = list(create_menu.values())[int(index) - 1]
+    class_args = []
+    pyautogui.hotkey('ctrl', ';')
+
+    for arg in args:
+        input_data = input(f'Please enter value for {arg}:')
+        class_args.append(input_data)
+
+    object_instance = class_instance(*class_args)
+
+    return list_of_weapon_objects.append(object_instance)
+
+
+def display_text_create_menu(class_dict: dict):
+    print("You are able to create instance of weapon object. You can chose between:")
+    text_options = ''
+    for index, key in enumerate(class_dict):
+        text_options += f'{index + 1} - {key}\n'
+    text_options += f'{len(class_dict) + 1} - Exit'
+
+    return print(text_options)
+
+
+def display_main_menu():
+    return print('You are able to:\n1 - Create a new instance\n2 - To list all instances\n3 - Exit ')
+
+
+def list_instances_menu(list_of_weapon_objects):
+    pyautogui.hotkey('ctrl', ';')
+    # available operations
+    print('These are available weapons. You are able to see detailed info by them, choosing the number in front of '
+          'them:\n')
+    for index, instance in enumerate(list_of_weapon_objects):
+        print(f"{index + 1} - {instance}")
+    exit_option = len(list_of_weapon_objects) + 1
+    print(f"{exit_option} - Return to previous menu")
+    input_data = input()
+    return
--- main.py
+++ main.py
@@ -1,76 +1,22 @@
 import sys
 import inspect
 import pyautogui
+
+from WeaponFactoryOOP.helpers import seed, display_main_menu, display_text_create_menu, list_instances_menu, \
+    get_classes_dictionary, create_instance_by_index
 from WeaponFactoryOOP.validations import test_input_value, ValueLowError, ValueHighError
-from WeaponFactoryOOP.class_library import Weapon, FireArms, ColdBladedWeapon, Axe, Sword
 
-
-def get_classes_dictionary():
-    cls_members = [member for member in inspect.getmembers(sys.modules[__name__], inspect.isclass) if member[1].__module__ == "WeaponFactoryOOP.class_library"]
-    classes = dict()
-
-    for cls in cls_members:
-        args = inspect.getfullargspec(cls[1].__init__)[0]
-        args.remove('self')
-        classes[cls[1].__doc__] = args
-
-    return classes, cls_members
+# add input validations
 
 
 list_of_weapon_objects = []
-
-
-def display_text_create_menu(class_dict: dict):
-    print("You are able to create instance of weapon object. You can chose between:")
-    text_options = ''
-    for index, key in enumerate(class_dict):
-        text_options += f'{index + 1} - {key}\n'
-    text_options += f'{len(class_dict) + 1} - Exit'
-
-    return print(text_options)
-
-
-def display_main_menu():
-    return print('You are able to:\n1 - Create a new instance\n2 - To list all instances\n3 - Exit ')
-
-
-def list_instances_menu():
-    pyautogui.hotkey('ctrl', ';')
-    # available operations
-    print('These are available weapons. You are able to see detailed info by them, choosing the number in front of '
-          'them:\n')
-    for index, instance in enumerate(list_of_weapon_objects):
-        print(f"{index + 1} - {instance}")
-    exit_option = len(list_of_weapon_objects) + 1
-    print(f"{exit_option} - Return to previous menu")
-    input_data = input()
-    return
-
-
-def create_instance(index: str, class_dict: list, create_menu: dict):
-    class_instance = class_dict[int(index) - 1][1]
-
-    args = list(create_menu.values())[int(index) - 1]
-    class_args = []
-    pyautogui.hotkey('ctrl', ';')
-
-    for arg in args:
-        input_data = input(f'Please enter value for {arg}:')
-        class_args.append(input_data)
-
-    print(len(class_args))
-    object_instance = class_instance(*class_args)
-
-    return list_of_weapon_objects.append(object_instance)
-
-
-# add input validations
 
 
 def run():
     create_menu, class_dict = get_classes_dictionary()
 
     display_main_menu()
+    seed(list_of_weapon_objects)
 
     while True:
         try:
@@ -94,7 +40,7 @@ def run():
             input_data = input("Please make your choice:")
 
             # create instance and return to main menu
-            create_instance(input_data, class_dict, create_menu)
+            create_instance_by_index(input_data, class_dict, create_menu, list_of_weapon_objects)
 
             pyautogui.hotkey('ctrl', ';')
             input_data = ''
@@ -102,7 +48,7 @@ def run():
 
         if input_data == '2':
             # list of instances and operations with them
-            list_instances_menu()
+            list_instances_menu(list_of_weapon_objects)
 
         if input_data == '3':
             input_data = 'stop'
