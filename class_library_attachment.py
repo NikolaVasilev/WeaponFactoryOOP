b36e80178f9739a632d4323780c0d45a12c425a7
NikolaVasilev <nikola@madcorp.info>
2022-08-22 17:21:26 +0300

    attachment class library

--- class_library_attachment.py
+++ class_library_attachment.py
@@ -2,7 +2,9 @@ from abc import ABC, abstractmethod
 
 
 class Base(ABC):
-    def __init__(self, model: str, weight: float, length: float, outer_diameter: float, inner_diameter: float, cal: str):
+    def __init__(self, type: str, model: str, weight: float, length: float, outer_diameter: float,
+                 inner_diameter: float, cal: str):
+        self._type = type
         self._model = model
         self._weight = weight
         self._length = length
@@ -21,21 +23,25 @@ class Base(ABC):
 
 
 class Attachment(Base):
-    def __init__(self, model, weight, length, outer_diameter: float, inner_diameter: float, cal):
-        super().__init__(model, weight, length, outer_diameter, inner_diameter, cal)
+    def __init__(self, type, model, weight, length, outer_diameter: float, inner_diameter: float, cal):
+        super().__init__(type, model, weight, length, outer_diameter, inner_diameter, cal)
 
     def _base_description(self):
         return ''
 
     def _is_mounted_as_string(self):
-        return f'The {self._model} is mounted' if self._is_mounted else f'The {self._model} is not mounted'
+        return f'The {self._type} {self._model} is mounted' if self._is_mounted else f'The {self._type} {self._model} is not mounted'
 
 
 class Suppressor(Attachment):
+    """Suppressor"""
+
     def __init__(self, model, weight, length, outer_diameter, inner_diameter, cal):
-        super().__init__(model, weight, length, outer_diameter, inner_diameter, cal)
+        super().__init__(self.__doc__, model, weight, length, outer_diameter, inner_diameter, cal)
 
 
 class Muzzle(Attachment):
+    """Muzzle"""
+
     def __init__(self, model, weight, length, outer_diameter, inner_diameter, cal):
-        super().__init__(model, weight, length, outer_diameter, inner_diameter, cal)
+        super().__init__(self.__doc__, model, weight, length, outer_diameter, inner_diameter, cal)
